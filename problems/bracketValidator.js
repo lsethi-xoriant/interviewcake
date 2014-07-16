cbecbf97881e11ba50fcdf4e6f1fb644cd7d60d5
Don Mamaril <mamarildon@gmail.com>
2014-07-16 11:39:44 -0700

    add gotcha

--- problems/bracketValidator.js
+++ problems/bracketValidator.js
@@ -12,6 +12,10 @@
  * "{ [ ] ( ) }" should return true
  * "{ [ ( ] ) }" should return false
  * "{ [ }" should return false
+ *
+ * Gotchas:
+ * Simply making sure each opener has a corresponding closer is not enough—we must also confirm that they are correctly ordered.
+ * For example, "{ [ ( ] ) }" should return false, even though each opener can be matched to a closer.
 */
 
 var bracketValidator = function () {
