50bcac552c30bb90e15c0265bfd553a3fffe8db2
Don Mamaril <mamarildon@gmail.com>
2014-07-22 20:20:24 -0700

    Add queueStack gotcha

--- problems/queueStacks.js
+++ problems/queueStacks.js
@@ -2,6 +2,9 @@
  * Implement a queue ↴ with 2 stacks ↴ . Your queue should have an enqueue and a dequeue function and it should be "first in first out" (FIFO).
  * Optimize for the time cost of m function calls on your queue. These can be any mix of enqueue and dequeue calls.
  * Assume you already have a stack implementation and it gives O(1) time push and pop.
+ * 
+ * Gotchas: 
+ * We can get O(m) runtime for m function calls. Crazy, right?
  */
 
 
